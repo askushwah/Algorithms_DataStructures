6b9f9e7a1d2a1326e52eb88b2f1c8ea3642990d3
Aditya Kushwah <adityakushwah@Adityas-MacBook-Pro.local>
2018-06-22 16:05:49 +0530

    Included time to calculate the time taken by algo

--- Dynamic Programming/CoinChange.py
+++ Dynamic Programming/CoinChange.py
@@ -1,10 +1,11 @@
 # This coin change problem will have three implementations which I have found on the internet. 
 # Diffrenet way to handle the same problem gives the ability to the programmer to think in many direction
-
+import time
 class CoinChange:
     def __init__(self, coin):
         self.coin = coin
     # Coin change solution using recursion
+    
     def change_recursion(self, amount, currentCoin):
         if amount == 0:
             return 1
@@ -15,16 +16,20 @@ class CoinChange:
             for i in range(currentCoin, len(self.coin)):
                 result += self.change_recursion(amount - self.coin[i], i)
         return result
+    
 
     # Coin change using Dynamic programming
     def change_dynamic(self, amount):
+        start_time_d = time.time()
         comibination = [0] * (amount+1)
         comibination[0] = 1
         for coin in self.coin:
             for i in range(1,amount+1):
                 if i >= coin:
                     comibination[i] += comibination[i-coin]
-        return comibination[amount]
+        end_time_d = time.time()
+        return comibination[amount],end_time_d-start_time_d
+    
     
     # Coin chamnge using Dynamic programming but this time the coins will be linear i.e. 1,2,3,4
     def change_dynamic_linear(self,amount):
@@ -39,11 +44,11 @@ class CoinChange:
         return result[len(self.coin)][amount]
 
 
-coin = [1,2]
-amount = 4
+coin = [5, 37, 8, 39, 33, 17, 22, 32, 13, 7, 10, 35, 40, 2, 43, 49, 46, 19, 41, 1, 12, 11, 28]
+amount = 166
 change = CoinChange(coin)
 # All the results will be same which checks the validity of the implementation
-print(change.change_recursion(amount, 0))
+#print(change.change_recursion(amount, 0))
 print(change.change_dynamic(amount))
-print(change.change_dynamic_linear(amount))
+#print(change.change_dynamic_linear(amount))
 
